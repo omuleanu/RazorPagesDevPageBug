c1a12d867c7c8c6f16a514bf8f0986a19f86b003
Val <vnexster@gmail.com>
2023-03-22 12:44:43 +0200

    devpage bug on razorpages demo

---
+++ MyLib/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text.Encodings.Web;
+
+namespace MyLib
+{
+    public class MyList : IHtmlContent
+    {
+        string render()
+        {
+            var res = JsonConvert.SerializeObject(new { Item1 = "abc" });
+            return "list";
+        }
+
+        public void WriteTo(TextWriter writer, HtmlEncoder encoder)
+        {
+            writer.Write(render());
+        }
+    }
+
+    public static class Helpers
+    {
+        public static MyList MyList1(this IHtmlHelper helper)
+        {
+            return new MyList();
+        }
+    }
+}
